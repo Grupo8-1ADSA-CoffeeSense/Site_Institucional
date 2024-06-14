2094e17c5d782319b762831816688cc1ba5ae8ff
Thomas-Albuquerque <thomas.albuquerque@sptech.school>
2024-06-14 01:17:51 -0300

    adicionando alert na homeDashboard

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -14,15 +14,15 @@ function autenticar(email, senha) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(email, nome, senha, empresaId) {
-    console.log("ACESSEI O USUARIO MODEL \n\n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n\t\t >> verifique suas credenciais de acesso ao banco\n\t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email,nome, senha, empresaId);
+    console.log("ACESSEI O USUARIO MODEL \n\n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n\t\t >> verifique suas credenciais de acesso ao banco\n\t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, nome, senha, empresaId);
     var instrucaoSql = `
       INSERT INTO usuario (email, nome, senha, fkEmpresa) VALUES ('${email}', '${nome}', '${senha}', ${empresaId});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
-  }
-  
-  function nomeUsuario(usuarioId) {
+}
+
+function nomeUsuario(usuarioId) {
     console.log("Acessando o método nome no modelo do usuário");
     var instrucaoSql = `
     SELECT usuario.nome  AS nome_Usuario
@@ -34,29 +34,33 @@ function cadastrar(email, nome, senha, empresaId) {
 }
 
 function obterArmazens(usuarioId) {
-  var instrucaoSql = `
-      SELECT 
-          a.idArmazem, 
-          a.nome AS nomeArmazem,  
-          a.localizacao, 
-          a.capacidade_toneladas,
-          e.nome AS nomeEmpresa
-      FROM 
-          armazem a
-      JOIN 
-          empresa e ON a.fkEmpresa = e.idEmpresa
-      JOIN 
-          usuario u ON e.idEmpresa = u.fkEmpresa
-      WHERE 
-          u.idUsuario = ${usuarioId};
-  `;
-  return database.executar(instrucaoSql);
+    var instrucaoSql =
+        ` SELECT 
+            a.idArmazem, 
+            a.nome AS nomeArmazem,  
+            a.localizacao, 
+            a.capacidade_toneladas,
+            e.nome AS nomeEmpresa,
+            dm.temperatura,
+            dm.umidade,
+            dm.data_horaCaptura AS dataHoraCaptura
+        FROM 
+            armazem a
+        JOIN 
+            empresa e ON a.fkEmpresa = e.idEmpresa
+        JOIN 
+            usuario u ON e.idEmpresa = u.fkEmpresa
+        LEFT JOIN 
+            dados_monitoramento dm ON a.idArmazem = dm.fkDispositivo
+        WHERE 
+            u.idUsuario = ${usuarioId};
+    ;`
+    return database.executar(instrucaoSql);
 }
-
 
 module.exports = {
     autenticar,
     cadastrar,
     nomeUsuario,
-    obterArmazens
+    obterArmazens,
 };
